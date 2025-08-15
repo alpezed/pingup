d4e086feb7552fe195ea14f73e70e3c17668b96e
Ed <alpezed@gmail.com>
2025-08-15 09:47:29 +0800

    add better auth session config

--- server/lib/auth.js
+++ server/lib/auth.js
@@ -1,62 +1,62 @@
-import { betterAuth } from "better-auth";
-import { mongodbAdapter } from "better-auth/adapters/mongodb";
-import { admin } from "better-auth/plugins";
+import { betterAuth } from 'better-auth';
+import { mongodbAdapter } from 'better-auth/adapters/mongodb';
+import { admin } from 'better-auth/plugins';
 
-import resend from "../config/resend.js";
-import db from "../config/db.js";
+import resend from '../config/resend.js';
+import db from '../config/db.js';
 
 export const auth = betterAuth({
 	database: mongodbAdapter(db),
 	plugins: [admin()],
+	session: {
+		cookie: {
+			secure: process.env.NODE_ENV === 'production',
+			httpOnly: true,
+		},
+	},
 	secret: process.env.BETTER_AUTH_SECRET,
-	appName: "pingup",
+	appName: 'pingup',
 	baseURL: process.env.BETTER_AUTH_URL,
 	emailAndPassword: {
 		enabled: true,
 		sendResetPassword: async ({ user, url, token }) => {
 			resend.emails.send({
-				from: "PingUp <edalpez@eddev.dev>",
+				from: 'PingUp <edalpez@eddev.dev>',
 				to: user.email,
-				subject: "Reset your password",
+				subject: 'Reset your password',
 				html: `Click the link to reset your password: ${url}`,
 			});
 		},
 	},
 	user: {
-		modelName: "users",
+		modelName: 'users',
 		additionalFields: {
 			username: {
-				type: "string",
+				type: 'string',
 				unique: true,
 			},
 			cover_photo: {
-				type: "string",
-				defaultValue: "",
+				type: 'string',
+				defaultValue: '',
 			},
 			bio: {
-				type: "string",
-				defaultValue: "",
+				type: 'string',
+				defaultValue: '',
 			},
 			location: {
-				type: "string",
-				defaultValue: "",
+				type: 'string',
+				defaultValue: '',
 			},
 			followers: {
-				type: "string[]",
+				type: 'string[]',
 				required: false,
 				defaultValue: [],
 			},
 			following: {
-				type: "string[]",
+				type: 'string[]',
 				required: false,
 				defaultValue: [],
 			},
 		},
 	},
-	// hooks: {
-	// 	after: async ctx => {
-	// 		console.log({ ctx });
-	// 		return ctx;
-	// 	},
-	// },
 });
