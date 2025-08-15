7c0112adb16a443174f9ca3f830ce127d762ca4d
Ed <alpezed@gmail.com>
2025-08-15 08:20:56 +0800

    update cors origin

--- server/app.js
+++ server/app.js
@@ -1,41 +1,41 @@
-import express from "express";
-import morgan from "morgan";
-import { APIError } from "better-auth/api";
-import cors from "cors";
-import { toNodeHandler } from "better-auth/node";
-import "dotenv/config";
+import express from 'express';
+import morgan from 'morgan';
+import { APIError } from 'better-auth/api';
+import cors from 'cors';
+import { toNodeHandler } from 'better-auth/node';
+import 'dotenv/config';
 
-import userRoutes from "./routes/user.route.js";
-import postRoutes from "./routes/post.route.js";
-import { auth } from "./lib/auth.js";
-import globalError from "./controllers/error.js";
+import userRoutes from './routes/user.route.js';
+import postRoutes from './routes/post.route.js';
+import { auth } from './lib/auth.js';
+import globalError from './controllers/error.js';
 
 const app = express();
 
-if (process.env.NODE_ENV === "development") {
-	app.use(morgan("dev"));
+if (process.env.NODE_ENV === 'development') {
+	app.use(morgan('dev'));
 }
 
 app.use(
 	cors({
-		origin: "http://localhost:3000",
-		methods: ["GET", "POST", "PUT", "DELETE"],
+		origin: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
+		methods: ['GET', 'POST', 'PUT', 'DELETE'],
 		credentials: true, // Allow cookies to be sent
-		allowedHeaders: ["Content-Type", "Authorization"],
+		allowedHeaders: ['Content-Type', 'Authorization'],
 	})
 );
 
-app.all("/api/auth/*splat", toNodeHandler(auth));
+app.all('/api/auth/*splat', toNodeHandler(auth));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/api/v1/users", userRoutes);
-app.use("/api/v1/posts", postRoutes);
+app.use('/api/v1/users', userRoutes);
+app.use('/api/v1/posts', postRoutes);
 
-app.all("*splat", (req, res, next) => {
+app.all('*splat', (req, res, next) => {
 	next(
-		new APIError("NOT_FOUND", {
+		new APIError('NOT_FOUND', {
 			message: `Can't find ${req.originalUrl} on the server!`,
 		})
 	);
