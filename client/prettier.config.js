b48c8ed0a1c21457f416f559a53e7f2632203ee5
Ed <alpezed@gmail.com>
2025-08-19 15:13:39 +0800

    added prettier

---
+++ client/prettier.config.js
@@ -0,0 +1,15 @@
+/** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
+const config = {
+  singleQuote: false,
+  jsxSingleQuote: false,
+  trailingComma: "all",
+  useTabs: false,
+  tabWidth: 2,
+  semi: true,
+  arrowParens: "always",
+  bracketSpacing: true,
+  quoteProps: "consistent",
+  printWidth: 80,
+};
+
+export default config;
