48b71da4a89cc91ed12fc2801e43571dd5c79be0
Ed <alpezed@gmail.com>
2025-08-19 20:57:49 +0800

    integrate stories carousel

--- client/src/components/stories.tsx
+++ client/src/components/stories.tsx
@@ -22,17 +22,31 @@ import { ToggleGroup, ToggleGroupItem } from "@/components/ui/toggle-group";
 import { Button } from "@/components/ui/button";
 import { cn, storyColors } from "@/lib/utils";
 import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+} from "@/components/ui/carousel";
 
 export default function Stories() {
   const { data: stories } = useSuspenseQuery(storyQueries.stories());
 
   return (
-    <div className="w-screen sm:w-[calc(100vw-240px)] lg:max-w-2xl  no-scrollbar overflow-x-auto px-4">
+    <div className="w-screen sm:w-[calc(100vw-240px)] lg:max-w-2xl px-4">
       <div className="flex gap-4 pb-5">
         <CreateStory />
-        {stories.data.map((story) => (
-          <StoryItem key={story._id} story={story} />
-        ))}
+        <Carousel className="w-[calc(100%-110px)]">
+          <CarouselContent>
+            {stories.data.map((story) => (
+              <CarouselItem
+                key={story._id}
+                className="md:basis-1/3 lg:basis-1/4 h-40"
+              >
+                <StoryItem story={story} />
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+        </Carousel>
       </div>
     </div>
   );
@@ -42,7 +56,7 @@ function StoryItem({ story }: { story: Story }) {
   return (
     <div
       className={cn(
-        "relative rounded-lg shadow min-w-30 max-w-30 max-h-40 cursor-pointer hover:shadow-lg transition-all duration-200 active:scale-95",
+        "relative rounded-lg shadow min-w-30 h-full max-w-30 max-h-40 cursor-pointer hover:shadow-lg transition-all duration-200 active:scale-95",
         story.background_color,
       )}
       style={{ backgroundColor: story.background_color }}
@@ -84,8 +98,6 @@ function CreateStory() {
     resolver: zodResolver(createStorySchema),
   });
 
-  console.log(form.formState.errors);
-
   const onSubmit = async (data: CreateStory) => {
     const formData = new FormData();
     formData.append("text", data.text);
@@ -104,106 +116,107 @@ function CreateStory() {
   };
 
   return (
-    <>
-      <Dialog open={open}>
-        <div
-          className="rounded-lg shadow-sm min-w-30 max-w-30 max-h-40 aspect-[3/4] cursor-pointer hover:shadow-lg transition-all duration-200 border-2 border-dashed border-indigo-300 bg-gradient-to-b from-indigo-50 to-white"
-          onClick={() => setOpen(true)}
-        >
-          <div className="h-full flex flex-col items-center justify-center p-4">
-            <div className="size-10 bg-indigo-500 rounded-full flex items-center justify-center mb-3">
-              <Plus size={24} className="w-5 h-5 text-white" />
-            </div>
-            <p className="text-sm font-medium text-slate-700 text-center">
-              Create Story
-            </p>
+    <Dialog open={open}>
+      <div
+        className="rounded-lg shadow-sm min-w-30 max-w-30 max-h-40 aspect-[3/4] cursor-pointer hover:shadow-lg transition-all duration-200 border-2 border-dashed border-indigo-300 bg-gradient-to-b from-indigo-50 to-white"
+        onClick={() => setOpen(true)}
+      >
+        <div className="h-full flex flex-col items-center justify-center p-4">
+          <div className="size-10 bg-indigo-500 rounded-full flex items-center justify-center mb-3">
+            <Plus size={24} className="w-5 h-5 text-white" />
           </div>
+          <p className="text-sm font-medium text-slate-700 text-center">
+            Create Story
+          </p>
         </div>
-        <DialogContent className="bg-transparent border-0 shadow-none text-white w-[448px] p-0 sm:max-w-xl my-2">
-          <DialogHeader className="relative flex items-center justify-center py-2">
-            <button
-              type="button"
-              onClick={() => setOpen(false)}
-              className="absolute left-4 top-2 cursor-pointer"
-            >
-              <ArrowLeft size={24} className="w-5 h-5 text-white" />
-            </button>
-            <DialogTitle>Create Story</DialogTitle>
-          </DialogHeader>
-          <DialogDescription>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
-              <div>
-                <Textarea
-                  {...form.register("text")}
-                  placeholder="What's on your mind?"
+      </div>
+      <DialogContent
+        showCloseButton={false}
+        className="bg-transparent border-0 shadow-none text-white w-[448px] p-0 sm:max-w-xl my-2"
+      >
+        <DialogHeader className="relative flex items-center justify-center py-2">
+          <button
+            type="button"
+            onClick={() => setOpen(false)}
+            className="absolute left-4 top-2 cursor-pointer"
+          >
+            <ArrowLeft size={24} className="w-5 h-5 text-white" />
+          </button>
+          <DialogTitle>Create Story</DialogTitle>
+        </DialogHeader>
+        <DialogDescription>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+            <div>
+              <Textarea
+                {...form.register("text")}
+                placeholder="What's on your mind?"
+                className={cn(
+                  "w-full border-0 !text-4xl p-6 rounded-xl bg-indigo-600 text-white placeholder:text-white/50 focus:ring-2 focus:ring-indigo-500 focus:border-transparent h-96",
+                  form.watch("background_color"),
+                )}
+              />
+            </div>
+            <div className="flex gap-2">
+              {storyColors.map((color) => (
+                <button
+                  key={color}
+                  type="button"
                   className={cn(
-                    "w-full border-0 !text-4xl p-6 rounded-xl bg-indigo-600 text-white placeholder:text-white/50 focus:ring-2 focus:ring-indigo-500 focus:border-transparent h-96",
-                    form.watch("background_color"),
+                    `w-6 h-6 rounded-full active:scale-105 transition hover:scale-125 border border-white ${color} hover:opacity-80 transition duration-150 cursor-pointer`,
+                    {
+                      "scale-125": form.watch("background_color") === color,
+                    },
                   )}
+                  onClick={() => form.setValue("background_color", color)}
                 />
-              </div>
-              <div className="flex gap-2">
-                {storyColors.map((color) => (
-                  <button
-                    key={color}
-                    type="button"
-                    className={cn(
-                      `w-6 h-6 rounded-full active:scale-105 transition hover:scale-125 border border-white ${color} hover:opacity-80 transition duration-150 cursor-pointer`,
-                      {
-                        "scale-125": form.watch("background_color") === color,
-                      },
-                    )}
-                    onClick={() => form.setValue("background_color", color)}
-                  />
-                ))}
-              </div>
-              <ToggleGroup
-                type="single"
-                variant="outline"
-                defaultValue="text"
-                className="w-full gap-3"
-                onValueChange={(value: "media" | "text") => {
-                  form.setValue("story_type", value);
-                }}
+              ))}
+            </div>
+            <ToggleGroup
+              type="single"
+              variant="outline"
+              defaultValue="text"
+              className="w-full gap-3"
+              onValueChange={(value: "media" | "text") => {
+                form.setValue("story_type", value);
+              }}
+            >
+              <ToggleGroupItem
+                value="text"
+                aria-label="Toggle text"
+                className="!rounded disabled:opacity-100 active:scale-95 !border-0 bg-primary transition duration-300 text-white h-10 cursor-pointer font-normal text-base"
+                disabled={form.watch("story_type") === "text"}
               >
-                <ToggleGroupItem
-                  value="text"
-                  aria-label="Toggle text"
-                  className="!rounded disabled:opacity-100 active:scale-95 !border-0 bg-primary transition duration-300 text-white h-10 cursor-pointer font-normal text-base"
-                  disabled={form.watch("story_type") === "text"}
-                >
-                  <AlignLeft className="h-4 w-4" />
-                  Text
-                </ToggleGroupItem>
-                <ToggleGroupItem
-                  value="media"
-                  aria-label="Toggle upload"
-                  className="!rounded !border-0 disabled:opacity-100 active:scale-95 bg-primary transition duration-300 text-white h-10 cursor-pointer font-normal text-base"
-                  disabled={form.watch("story_type") === "media"}
-                >
-                  <Upload className="h-4 w-4" />
-                  <span>Photo/Video</span>
-                </ToggleGroupItem>
-              </ToggleGroup>
-              <DialogFooter>
-                <Button
-                  type="submit"
-                  className="text-base w-full h-12 font-normal px-4 py-2  text-white rounded-lg disabled:opacity-95 hover:from-indigo-600 hover:to-purple-700 transition cursor-pointer disabled:bg-gray-500"
-                  disabled={
-                    form.formState.isSubmitting ||
-                    (form.watch("story_type") === "media" &&
-                      !form.watch("medias")?.length) ||
-                    (form.watch("story_type") === "text" && !form.watch("text"))
-                  }
-                >
-                  <Sparkle size={20} className="!w-4.5 !h-4.5" />
-                  Create Story
-                </Button>
-              </DialogFooter>
-            </form>
-          </DialogDescription>
-        </DialogContent>
-      </Dialog>
-    </>
+                <AlignLeft className="h-4 w-4" />
+                Text
+              </ToggleGroupItem>
+              <ToggleGroupItem
+                value="media"
+                aria-label="Toggle upload"
+                className="!rounded !border-0 disabled:opacity-100 active:scale-95 bg-primary transition duration-300 text-white h-10 cursor-pointer font-normal text-base"
+                disabled={form.watch("story_type") === "media"}
+              >
+                <Upload className="h-4 w-4" />
+                <span>Photo/Video</span>
+              </ToggleGroupItem>
+            </ToggleGroup>
+            <DialogFooter>
+              <Button
+                type="submit"
+                className="text-base w-full h-12 font-normal px-4 py-2  text-white rounded-lg disabled:opacity-95 hover:from-indigo-600 hover:to-purple-700 transition cursor-pointer disabled:bg-gray-500"
+                disabled={
+                  form.formState.isSubmitting ||
+                  (form.watch("story_type") === "media" &&
+                    !form.watch("medias")?.length) ||
+                  (form.watch("story_type") === "text" && !form.watch("text"))
+                }
+              >
+                <Sparkle size={20} className="!w-4.5 !h-4.5" />
+                Create Story
+              </Button>
+            </DialogFooter>
+          </form>
+        </DialogDescription>
+      </DialogContent>
+    </Dialog>
   );
 }
