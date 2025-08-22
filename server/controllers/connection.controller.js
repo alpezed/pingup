ef29712f1498828da5a367245cc361ea87e85b24
Ed <alpezed@gmail.com>
2025-08-22 15:19:42 +0800

    build connections count endpoint

--- server/controllers/connection.controller.js
+++ server/controllers/connection.controller.js
@@ -1,5 +1,7 @@
 import { catchAsync } from '../utils/catch-async.js';
 import Connection from '../models/connection.model.js';
+import db from '../config/db.js';
+import { ObjectId } from 'mongodb';
 
 export const addConnection = catchAsync(async (req, res) => {
 	// check if user has sent more than 20 requests in the last 24 hours
@@ -17,12 +19,12 @@ export const addConnection = catchAsync(async (req, res) => {
 		});
 	}
 
-	// if (await Connection.isConnected(req.user.id, req.body.id)) {
-	// 	return res.status(200).json({
-	// 		success: true,
-	// 		message: 'You are already connected',
-	// 	});
-	// }
+	if (await Connection.isConnected(req.user.id, req.body.id)) {
+		return res.status(200).json({
+			success: true,
+			message: 'You are already connected',
+		});
+	}
 
 	if (!(await Connection.hasPendingRequest(req.user.id, req.body.id))) {
 		await Connection.create({ from_user: req.user.id, to_user: req.body.id });
@@ -55,5 +57,51 @@ export const getAllConnections = catchAsync(async (req, res) => {
 		success: true,
 		total,
 		data: connections,
+	});
+});
+
+export const getConnectionCounts = catchAsync(async (req, res) => {
+	const [totalConnections, pendingRequests, totalFollowers, totalFollowing] =
+		await Promise.all([
+			Connection.countDocuments({
+				from_user: req.user.id,
+				status: 'accepted',
+			}),
+			Connection.countDocuments({
+				to_user: req.user.id,
+				status: 'pending',
+			}),
+			db
+				.collection('users')
+				.aggregate([
+					{ $match: { _id: new ObjectId(req.user.id) } },
+					{ $project: { followersCount: { $size: '$followers' } } },
+				])
+				.toArray()
+				.then(result => {
+					console.log({ result });
+					return result[0] ? result[0].followersCount : 0;
+				}),
+			db
+				.collection('users')
+				.aggregate([
+					{ $match: { _id: new ObjectId(req.user.id) } },
+					{ $project: { followingCount: { $size: '$following' } } },
+				])
+				.toArray()
+				.then(result => {
+					console.log({ result });
+					return result[0] ? result[0].followingCount : 0;
+				}),
+		]);
+
+	res.status(200).json({
+		success: true,
+		data: {
+			connections: totalConnections,
+			pending: pendingRequests,
+			followers: totalFollowers,
+			following: totalFollowing,
+		},
 	});
 });
--- server/routes/connection.route.js
+++ server/routes/connection.route.js
@@ -4,11 +4,14 @@ import { protect } from '../middleware/auth.middleware.js';
 import {
 	addConnection,
 	getAllConnections,
+	getConnectionCounts,
 } from '../controllers/connection.controller.js';
 
 const router = express.Router();
 
 router.use(protect);
+
+router.get('/connection-counts', getConnectionCounts);
 
 router.route('/').get(getAllConnections).post(addConnection);
 
